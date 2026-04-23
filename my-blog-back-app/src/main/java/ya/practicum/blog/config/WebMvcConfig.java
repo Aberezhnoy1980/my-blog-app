267597a764855886f3e44886641fd42758806dec
Aleksandr <berezhnoy_aa@mail.ru>
2026-04-23 13:25:59 +0300

    refactor: migrate backend runtime to spring boot executable jar

--- my-blog-back-app/src/main/java/ya/practicum/blog/config/WebMvcConfig.java
+++ my-blog-back-app/src/main/java/ya/practicum/blog/config/WebMvcConfig.java
@@ -8,11 +8,9 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final LocalValidatorFactoryBean validatorFactoryBean;
